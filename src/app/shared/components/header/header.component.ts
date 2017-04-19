2eeba65adf9c965e7313217b6529f210ad0d10cc
Jose Centeno <jmcenteno@gmail.com>
2017-04-19 19:44:21 -0400

    app header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { APP_NAME } from '../../../app.config';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  appName: String = APP_NAME;
 
   constructor() { }
 
