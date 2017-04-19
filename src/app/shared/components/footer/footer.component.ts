6889507ce4ef0e57be9eadccdb434e9084c0e91a
Jose Centeno <jmcenteno@gmail.com>
2017-04-19 19:43:40 -0400

    footer component

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { APP_NAME } from '../../../app.config';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  appName: String = APP_NAME;
+  year: Number = (new Date()).getFullYear();
 
   constructor() { }
 
