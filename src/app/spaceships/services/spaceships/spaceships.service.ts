8983115d4eb4095f3b395a4e19cbaa48e05cec50
Jose Centeno <jmcenteno@gmail.com>
2017-04-20 11:51:50 -0400

    spaceships service

--- src/app/spaceships/services/spaceships/spaceships.service.ts
+++ src/app/spaceships/services/spaceships/spaceships.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable, BehaviorSubject } from 'rxjs/Rx';
+import * as _ from 'lodash';
+
+import { API_URL } from '../../../app.config';
+import { Spaceship } from './spaceship';
 
 @Injectable()
 export class SpaceshipsService {
 
-  constructor() { }
+  private spaceships$: BehaviorSubject<Spaceship[]> = new BehaviorSubject<Spaceship[]>(null);
+
+  constructor(private http: Http) { }
+
+  get spaceships(): Observable<Spaceship[]> {
+
+    return this.spaceships$;
+
+  }
+
+  getAllSpaceships(): void {
+
+    this.http.get(`${API_URL}/spaceships`)
+      .subscribe(response => {
+        this.spaceships$.next(response.json().products || []);
+      });
+
+  }
+
+  getSpaceshipByName(name: String): Observable<Spaceship> {
+
+    return _.find(this.spaceships$, { name: name });
+
+  }
 
 }
