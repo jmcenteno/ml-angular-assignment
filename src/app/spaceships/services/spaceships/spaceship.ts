78e81602bb5fbe536f2466eef765ccd99e5e82d5
Jose Centeno <jmcenteno@gmail.com>
2017-04-21 02:51:56 -0400

    added price to model

--- src/app/spaceships/services/spaceships/spaceship.ts
+++ src/app/spaceships/services/spaceships/spaceship.ts
@@ -3,6 +3,7 @@ import { TechSpecs } from './tech-specs';
 export class Spaceship {
     id: Number;
     name: String;
+    price: String;
     manufacturer: String;
     class: String;
     techspecs: TechSpecs;
